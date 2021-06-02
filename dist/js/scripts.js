b311111871ea4365e343175c05f7b366e73dbf90
elkgrovebikepark <83841036+elkgrovebikepark@users.noreply.github.com>
2021-06-02 12:58:16 -0700

    Update scripts.js

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -52,5 +52,9 @@
     navbarCollapse();
     // Collapse the navbar when page is scrolled
     $(window).on('scroll', navbarCollapse);
+  
+    $(".identifyingClass").click(function () {
+            var my_id_value = $(this).data('id');
+            $(".modal-body #hiddenValue").val(my_id_value);
 
 })(jQuery); // End of use strict
